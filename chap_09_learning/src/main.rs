d01858edd1d3463dfc622f32c4852f3c60dcb9ec
jkblr <joel_k@hotmail.de>
2026-04-24 21:07:34 +0200

    Chapter 9 stuff

---
+++ chap_09_learning/src/main.rs
@@ -0,0 +1,36 @@
+// fn main() {
+//    let v = vec![1, 2, 3];
+//
+//    v[99];
+//}
+
+//use std::fs::File;
+
+//fn main() {
+//    let greeting_file_result = File::open("hello.txt");
+//
+//    let greeting_file = match greeting_file_result {
+//        Ok(file) => file,
+//        Err(error) => panic!("Problem opening the file: {error:?}"),
+//    };
+//}
+
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening the file: {error:?}");
+            }
+        },
+    };
+}
